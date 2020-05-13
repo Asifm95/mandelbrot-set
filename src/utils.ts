fcbf6b9a2b131f3a991398613ea6c2705255abb3
Asif Mohammed <asifm2882@gmail.com>
2020-05-13 20:45:10 +0530

    feat: added renormalizing fn :sparkles:

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,8 @@ export const tranformToXY = (
   height: number,
   scale: number
 ) => [xCenter + (j - width / 2) / scale, yCenter + (-i + height / 2) / scale];
+
+export const renormalizeEscape = (n: number, modulus: number) => {
+  const norm = n + 1 - Math.log(Math.log2(modulus));
+  return isNaN(norm) ? 100 : norm;
+};
