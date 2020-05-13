ac3fb45ca6c2394984af3afc366465b8136d33a6
Asif Mohammed <asifm2882@gmail.com>
2020-05-13 14:30:10 +0530

    feat: setup & draw mandelbrot :sparkles:

---
+++ src/render.ts
@@ -0,0 +1,47 @@
+import config from './config';
+import p5 from 'p5';
+import { tranformToXY } from './utils';
+import mandelbrot from './mandelbrot';
+
+let scale = 300;
+let scaleFactor = 2;
+let xCent = 0;
+let yCent = 0;
+const { width, height, maxItration } = config;
+
+const sketch = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(width, height);
+    p.noLoop();
+  };
+
+  p.draw = () => drawMandelbrot();
+
+  const drawMandelbrot = () => {
+    console.log('draw', width, height);
+    for (let i = 0; i <= height; i++) {
+      for (let j = 0; j <= width; j++) {
+        const [x, y] = tranformToXY(j, i, xCent, yCent, width, height, scale);
+        const [r, g, b] = getColor(mandelbrot(x, y));
+        p.stroke(p.color(r, g, b));
+        p.point(j, i);
+      }
+    }
+  };
+
+  const getColor = (iterationCount: number): [number, number, number] => {
+    if (iterationCount === maxItration) return [0, 0, 0];
+    else if (iterationCount <= maxItration / 2)
+      return [0, 0, 20 + Math.round(200 / (maxItration / 2)) * iterationCount];
+    else
+      return [
+        Math.round(200 / (maxItration / 2)) *
+          (iterationCount - Math.round(maxItration / 2)),
+        Math.round(200 / (maxItration / 2)) *
+          (iterationCount - Math.round(maxItration / 2)),
+        255,
+      ];
+  };
+};
+
+new p5(sketch);
